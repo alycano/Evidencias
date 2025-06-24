0f0ba1a6e0d422070b40fb8c8d670a603a78234d
Aly Santiago Cano <142368817+alycano@users.noreply.github.com>
2025-06-24 15:01:50 -0500

    Add files via upload

---
+++ Procesador_De_Texto/Procesador_De_Texto.js
@@ -0,0 +1,12 @@
+let texto = prompt("Escribe un texto:");
+const saludo = "Tú texto es: ";
+let Longitud_Texto = texto.length > 10;
+var mensaje = saludo + texto;
+var Variable_Nula = null;
+let negacion = !Longitud_Texto;
+
+console.log(mensaje);
+console.log(Longitud_Texto);
+console.log(negacion);
+console.log(Variable_Nula);
+
